f9602b9603ab857cd048eec4ed3391e0d899d0de
sashazakatov <58004989+sashazakatov@users.noreply.github.com>
2023-05-18 15:01:42 +0300

    Update BaseUnemployedList.jsx

--- src/components/BaseUnemployedList/BaseUnemployedList.jsx
+++ src/components/BaseUnemployedList/BaseUnemployedList.jsx
@@ -1,15 +1,17 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getBaseUnemployed } from "store/selectors";
 import { removeItem } from 'store/baseUnemployedSlice';
 import { NavLink, useLocation } from "react-router-dom";
 
 import { AiFillEye } from 'react-icons/ai';
 import { ImBin } from 'react-icons/im';
 
+import { getBaseUnemployed, getFilter } from "store/selectors";
+
 const BaseUnemployedList = () => {
     const location = useLocation();
     const unemployed = useSelector(getBaseUnemployed);
+    const filter = useSelector(getFilter);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -18,7 +20,9 @@ const BaseUnemployedList = () => {
 
     return(
        <ul>
-            { unemployed.map(({id, name }) => 
+            { unemployed
+            .filter(({name}) => name.toLowerCase().includes(filter.toLowerCase()))
+            .map(({id, name }) => 
             <li key={id}>
                 <p>{ name }</p>
                 <NavLink to={`/BaseJob/${id}`} state={{form: location}}>
